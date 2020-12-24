92fc814bd3ea812ed325090c75d8e717fdf049a1
Lê Thanh Bình <lethanhbinh3920@gmail.com>
2020-12-24 10:42:32 +0700

    Add code switch layout

--- app/src/main/java/com/example/tbm/SignUpActivity.java
+++ app/src/main/java/com/example/tbm/SignUpActivity.java
@@ -1,24 +1,24 @@
 package com.example.tbm;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.TextView;
 import android.content.Intent;
 import android.view.View;
+import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sign_in);
+        setContentView(R.layout.sign_up);
 
-        TextView btn = (TextView) findViewById(R.id.toRegisButton);
+        TextView btn = (TextView) findViewById(R.id.toSignInButton);
         btn.setOnClickListener(this);
     }
 
     public void onClick(View v) {
-        Intent intent = new Intent(this, SignUpActivity.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
