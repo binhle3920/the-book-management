84d32dc8bf90fe0e967a318f3b5e49097ce3682d
Thanh Thảo <51152007+baohuyen1411@users.noreply.github.com>
2020-12-28 16:11:15 +0700

    checkoutcart

---
+++ app/src/main/java/com/example/tbm/Checkout_activity.java
@@ -0,0 +1,14 @@
+package com.example.tbm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Checkout_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.checkout_activity);
+    }
+}
